f44bb310251cea19fe09dafa31a776a2f7b2a61a
Munshi Harisahmed Ayubkhan <142170103+Harismunshi@users.noreply.github.com>
2023-10-17 09:44:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function changeColor(){
+
+    let bgcolor = document.querySelector("#text").value;
+    
+    document.body.style.backgroundImage = "linear-gradient(45deg,"+bgcolor+")";
+    
+    
+    }
